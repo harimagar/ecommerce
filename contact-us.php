166fe20d30243a62b75f9eec8077f2b13ec61f7b
harimagar <magarharee54@gmail.com>
2024-12-25 11:06:48 +0545

    Created contact page

---
+++ contact-us.php
@@ -0,0 +1,20 @@
+		<?php include('header.php');?>
+        <?php include('menu.php');?>
+        <?php include('slider.php');?>
+       
+       	<div class="content">
+            <div class="content">
+            	<h1>Contact Us</h1>
+                <p>College Lane, Hatfield, </p>
+                <p>Herts AL10 9AB, United Kingdom</p>
+                <p>Phone: +447340969989</p>
+                <p>Email: info@ecommerce.com</p>
+        	</div>
+
+
+        </div> <!--end of content div-->
+       
+       <?php include('footer.php');?>
+    </div> <!--end of wrapper div-->
+</body>
+</html>
