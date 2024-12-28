3fbbaaabe59b18b4ec51d45b7e910a5df06b94d2
harimagar <magarharee54@gmail.com>
2024-12-28 19:07:02 +0545

    Created about us page

---
+++ about-us.php
@@ -0,0 +1,40 @@
+		<?php include('header.php');?>
+        <?php include('menu.php');?>
+        <?php include('slider.php');?>
+       
+       	<div class="content">
+            <div class="content">
+            	<h1>About Us - Recommendation System and E-Commerce </h1>
+            
+                
+                <h2>Introduction</h2>
+                <p>
+                In the modern and technological age, E-Commerce has played a significant role in revolutionising the way people shop. There are varieties of products that users can easily explore, compare, and buy from E-Commerce platforms, pay the cost online, and get delivered to their doorsteps. However, there is an issue. E-commerce platforms offer consumers all possible products, including groceries, electronics, vehicles, clothes, etc. Therefore, it can be difficult for users to find what they need and challenging for the E-Commerce platform to filter and show only relevant products to users to enhance their shopping experience. This issue can be solved by implementing a personalised recommendation system to analyse user purchase history and behaviour from past data and provide personalised products they will likely buy. This helps to enhance the user experience while using the e-commerce platform, helps them gain competitive benefits in this highly competitive market, and also helps to grow sales and revenue. 
+                </p>  
+                <p>
+                In the competitive e-commerce landscape, personalised recommendation systems have become crucial for enhancing user experience and boosting sales. This project aims to develop a recommendation system utilising machine learning algorithms to analyse customer behaviour and product preferences. By leveraging the e-commerce purchase history from the Electronics Store dataset, the project will deliver accurate and relevant product recommendations to improve customer engagement and conversion rates significantly.
+                </p>  
+                <p>
+                Apart from the recommendation system, a prototype of an E-Commerce platform will also be developed using modern web technologies for this project. The prototype will help to obtain the live system dataset for each year. The live system dataset will be a valuable asset to understanding the complexities and behaviour of real-world recommendation systems. This will also help to understand user interactions deeply and optimise the recommendation system accordingly.
+                </p>            
+
+                <h2>Objectives</h2>
+                <p>
+                    <ul>
+                        <li>Conduct a comprehensive literature review on recommendation systems and optimisation techniques.</li>
+                        <li>Acquire and pre-process the E-commerce Purchase History from the Electronics Store dataset.</li>
+                        <li>Develop and train various machine learning models, including collaborative filtering, content-based, and hybrid models.</li>
+                        <li>Analyze model performance based on defined metrics: accuracy, diversity, and customer engagement.</li>
+                        <li>Develop a prototype of an E-Commerce platform using modern web technologies to collect real-world data.</li>
+                        <li>Document the findings and prepare a final report summarising the project outcomes and recommendations.</li>
+                    </ul> 
+                </p>
+        	</div>
+
+
+        </div> <!--end of content div-->
+       
+       <?php include('footer.php');?>
+    </div> <!--end of wrapper div-->
+</body>
+</html>
